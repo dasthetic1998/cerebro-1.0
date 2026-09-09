e5599509b1285f44d44337e51ef539c3fdfb45bb
dasthetic1998 <alexanderalulima1998@gmail.com>
2026-09-09 05:55:58 +0200

    Update service worker caching logic

--- sw.js
+++ sw.js
@@ -1,11 +1,7 @@
 self.addEventListener("install", e => {
- e.waitUntil(
-  caches.open("safe-brain").then(cache => cache.addAll(["./"]))
- );
+ e.waitUntil(caches.open("zero").then(c => c.addAll(["./"])));
 });
 
 self.addEventListener("fetch", e => {
- e.respondWith(
-  caches.match(e.request).then(r => r || fetch(e.request))
- );
+ e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
