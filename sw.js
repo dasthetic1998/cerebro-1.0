0d20a2729c36bd223ba577eb18cb0a0aa3ec0695
dasthetic1998 <alexanderalulima1998@gmail.com>
2026-09-09 06:01:06 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener("install", e => {
- e.waitUntil(caches.open("zero").then(c => c.addAll(["./"])));
-});
-
-self.addEventListener("fetch", e => {
- e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
